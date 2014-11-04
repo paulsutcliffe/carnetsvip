d2a739417acb3838b17d613989a91c30f1c9d09b
Paul Sutcliffe <paul@kosmyka.com>
2014-11-04 10:25:58 -0500

    password

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,8 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'kosmyka.com', user: 'paul', port: 22, roles: %w{web app db}
+set :password, ask('Server password', nil)
+server 'kosmyka.com', user: 'paul', port: 22, password: fetch(:password), roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
